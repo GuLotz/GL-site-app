ce2d242b7f38144e6f8f3dc621877d055fdccc9d
Gunnar Lorentzen <gunnar@lorentzen-mit-tz.de>
2018-01-28 19:09:26 +0100

    The navigation tabs are now taken from the appRoutes array. Solution is not the nicest possible, but nicer than the one before.

--- src/app/navigation/app.navigationComponent.ts
+++ src/app/navigation/app.navigationComponent.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { appRoutes } from '../routing/app.routing';
 
 @Component({
   selector: 'navigation',
-  templateUrl: 'navigation.html',
+  templateUrl: 'navigation.html'
 })
 export class navigationComponent {
+  appRoutes: object;
   constructor(){
-
+    this.appRoutes = appRoutes;
+    this.appRoutes.splice(0,1);
+    this.appRoutes.splice(this.appRoutes.length-1,1)
   }
 };
