d4bf095b9a67c017618c33389e4ee0b86c33a223
Gunnar Lorentzen <gunnar@lorentzen-mit-tz.de>
2018-01-28 19:24:31 +0100

    added some comments

--- src/app/navigation/app.navigationComponent.ts
+++ src/app/navigation/app.navigationComponent.ts
@@ -9,6 +9,9 @@ export class navigationComponent {
   appRoutes: object;
   constructor(){
     this.appRoutes = appRoutes;
+    // this is dirty. I know here, that I do not want to show path'' and path'**' and I know their position
+    // throws an error, as appRoutes is not an array, but an object
+    // Works anyway in the transpiled JS
     this.appRoutes.splice(0,1);
     this.appRoutes.splice(this.appRoutes.length-1,1)
   }
